4b7a9c6be1a63b9a78b5b49a2fd7a13f4dfb0d7e
Victor Alves <victormatheusbotassoli@gmail.com>
2021-04-24 14:15:12 -0300

    Aula3

---
+++ 01-HTML-CSS-JS/aula-3/js/app.js
@@ -0,0 +1,52 @@
+
+$(document).ready(() => {
+    refreshTableSize();
+})
+
+$("#form-noticia").submit(function() {
+    try {
+        var json = recordFromForm($(this));
+        addDataTable(json);
+        refreshTableSize()
+        
+    } catch (e){
+        console.error(e);
+    }
+    return false;
+})
+
+function recordFromForm(form){
+    var inputs = form.find('input[type="text"], textarea');
+    var json = "";
+    inputs.each(function(idx, input){
+        var name = $(input).attr("name");
+        var value = $(input).val();
+        if (json !== "")
+            json += ",";
+        
+        json += `"${name}": "${ value.trim() }"`;
+    });
+    json = `{${json}}`;
+    return JSON.parse(json);
+}
+
+function addDataTable(noticiajson){
+    var tbody = $("#table-noticias tbody");
+    var tr = $("<tr></tr>");
+    var tdTitulo = $(`<td>${noticiajson['titulo']}</td>`);
+    var tdIntroducao = $(`<td>${noticiajson['introducao']}</td>`);
+    var tdAcao = $("<td><button type=\"button\" class=\"btn btn-danger\" onclick=\"remove(this)\">Remover</button></td>");
+
+    tr.append(tdTitulo, tdIntroducao, tdAcao);
+    tbody.append(tr);
+}
+
+function refreshTableSize() {
+    $("#num-row").text($("#table-noticias tbody tr").length)
+}
+
+function remove(element) {
+    $(element).parents('tr').remove();
+    refreshTableSize();
+}
+
